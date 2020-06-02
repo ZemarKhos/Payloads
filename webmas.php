2ceaad5c6afdb33c32652a6f9fe16fb1c01db87a
ZemarKhos <zshadow@protonmail.ch>
2020-06-02 12:50:47 +0300

    Update webmas.php

--- webmas.php
+++ webmas.php
@@ -1,82 +1,13 @@
-
+<!DOCTYPE HTML>
 <html>
-  <link rel="shortcut icon" type="image/x-icon" href="http://exelance.org/loca/data/avatars/m/0/1.jpg">
-  <link rel="mask-icon" type="" href="http://exelance.org/loca/data/avatars/m/0/1.jpg" color="#111">
-  <title>EXELANCE.ORG Design By Zemarchus</title>
-<script>
-(function titleScroller(text) {
-    document.title = text;
-    setTimeout(function () {
-        titleScroller(text.substr(1) + text.substr(0, 1));
-    }, 500);
-}("| Anonymous - #OpJustice4Floyd"));
-</script>
-<title>
-</title>
-
+<body background="http://i.hizliresim.com/mzpZny.jpg">
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Share+Tech+Mono">
-<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Geo">
-<meta content="We Are Anonymous"   name="description"/>
-<meta content="We Are Legion"  name="keywords"/>
-<meta content="We Do Not Forget, We Do Not Forgive"  name="Abstract"/>
-<meta name="Expect Us..."/>
+<title>Anonymous - #OpJustice4Floyd</title>
 </head>
-<style type="text/css" media="all">
-html,body{margin:0;padding:0;}
-#text-shadow-box{position:fixed;left:0;right:0;top:0;bottom:0;width:100%;height:100%;overflow:hidden;background:#ffffff;font-family:Geo,Stencil,Arial,sans-serif;-webkit-tap-highlight-color:rgba(0,0,0,0);-webkit-user-select:none;}
-#text-shadow-box #tsb-text,#text-shadow-box #tsb-link{position:absolute;top:49%;left:0;width:100%;height:1em;margin:-0.77em 0 0 0;font-size:70px;line-height:1em;font-weight:bold;text-align:center;}
-#text-shadow-box #tsb-text{font-size:100px;color:transparent;text-shadow:black 0px -45.2px 19px;}
-#text-shadow-box #tsb-link a{color:#FFF;text-decoration:none;}
-#text-shadow-box #tsb-box,#text-shadow-box #tsb-wall{position:absolute;top:50%;left:0;width:100%;height:60%;}
-#text-shadow-box #tsb-box{-webkit-box-shadow:black 0px -45.2px 39px;-moz-box-shadow:black 0px -45.2px 39px;}
-#text-shadow-box #tsb-wall{background:#ffffff;}
-#text-shadow-box #tsb-wall p{position:relative;font-size:15px;line-height:1.5em;text-align:justify;color:#222;width:550px;margin:1.5em auto;cursor:default;}
-#text-shadow-box #tsb-wall p a{color:#fff;}
-#text-shadow-box #tsb-wall p a:hover{text-decoration:none;color:#000;background:#fff;}
-#tsb-spot{position:absolute;top:-50%;left:-50%;width:200%;height:200%;pointer-events:none;background:-webkit-gradient(radial,center center,0,center center,450,from(rgba(0,0,0,0)),to(rgba(0,0,0,1)));background:-moz-radial-gradient(center 45deg,circle closest-side,transparent 0,black 450px);}
-#blue{color:#0062ff;}
-</style>
-<!--    [if IE]>
-<style type="text/css">
-/* Sadly no IE9 support for pointer-events: none; nor CSS2 text-shadow */
-#tsb-spot {
-    display: none;
-}
-#tsb-ie {
-    position: absolute;
-    top: -90%;
-    left: -50%;
-    width: 200%;
-    height: 334%;
-    background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPHJhZGlhbEdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgY3g9IjUwJSIgY3k9IjUwJSIgcj0iNzUlIj4KICAgIDxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiMwMDAwMDAiIHN0b3Atb3BhY2l0eT0iMCIvPgogICAgPHN0b3Agb2Zmc2V0PSI3NCUiIHN0b3AtY29sb3I9IiMwMDAwMDAiIHN0b3Atb3BhY2l0eT0iMSIvPgogICAgPHN0b3Agb2Zmc2V0PSIxMDAlIiBzdG9wLWNvbG9yPSIjMDAwMDAwIiBzdG9wLW9wYWNpdHk9IjEiLz4KICA8L3JhZGlhbEdyYWRpZW50PgogIDxyZWN0IHg9Ii01MCIgeT0iLTUwIiB3aWR0aD0iMTAxIiBoZWlnaHQ9IjEwMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
-}
-</style>
-<![endif]    --> <iframe width="1" height="1" src="https://www.youtube.com/embed/yVIRcnlRKF8?rel=0&autoplay=1&loop=1&playlist=yVIRcnlRKF8" frameborder="0" allowfullscreen></iframe> </center>
-
-<div id="text-shadow-box">
-  
-  <div style="box-shadow: 0px 75.2px 57px black;" id="tsb-box">
-     
-  </div></center>
-   
-  <p style="text-shadow: -112.8px 75.2px 37px black;" id="tsb-text">By Zemarchus</p>
-   
-  <p id="tsb-link">
-     <a href="#" target="_blank">By Zemarchus</a> 
-  </p>
-   
-  <div id="tsb-wall">
-     
-    <div id="tsb-ie">
-       
-    </div>
-     
-    <p>
-       
-    </p>
-     
+<div>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<div style="text-align:center"><img src="http://i.hizliresim.com/j2pRkJ.png" width="350" height="350" ></div>
+<br/><br/>
     <center>
 
 <h2>Anonymous - #OpJustice4Floyd</h2>
@@ -85,118 +16,7 @@ html,body{margin:0;padding:0;}
 <h2>We Do Not Forget, We DO Not Forgive</h2>
 <h5>Expect US</h5>
 </center>
-
-     
-
-     
-    <div id="ttecleado" ?="">
-       
-      
-         
-        <table>
-           
-          <tbody>
-             
-            <tr>
-               
-              <td colspan="2">
-                 
-                
-                 
-              </td>
-               
-              <td colspan="3">
-
-                
-              </td>
-               
-            </tr>
-             
-            <tr>
-               
-            </tr>
-             
-          </tbody>
-           
-        </table>
-         
-      </center>
-       
-    </div><br /> 
-  </div><span style="color: rgb(0, 98, 255); font-family: Tahoma; font-size: 18px;"><strong></strong></span> 
-  <div style="background-position: 282px -284px;" id="tsb-spot">
-     
-  </div><span style="color: rgb(0, 98, 255); font-family: Tahoma; font-size: 18px;"><strong></strong></span>
+<iframe width="1" height="1" src="https://www.youtube.com/embed/yVIRcnlRKF8?rel=0&autoplay=1&loop=1&playlist=yVIRcnlRKF8" frameborder="0" allowfullscreen></iframe></style>
 </div>
-
-
-<script type="text/javascript" language="javascript" charset="utf-8">
-/**
- * 
- * 
- * just happy (^_^)
- *
- * Power . . . of Anonymous/ . . .
- */
-var text = null;
-var spot = null;
-var box = null;
-var boxProperty = '';
-init();
-function init() {
-    text = document.getElementById('tsb-text');
-    spot = document.getElementById('tsb-spot');
-    box = document.getElementById('tsb-box');
-    if (typeof box.style.webkitBoxShadow == 'string') {
-        boxProperty = 'webkitBoxShadow';
-    } else if (typeof box.style.MozBoxShadow == 'string') {
-        boxProperty = 'MozBoxShadow';
-    } else if (typeof box.style.boxShadow == 'string') {
-        boxProperty = 'boxShadow';
-    }
-    if (text && spot && box) {
-        document.getElementById('text-shadow-box').onmousemove = onMouseMove;
-        document.getElementById('text-shadow-box').ontouchmove = function (e) {e.preventDefault(); e.stopPropagation(); onMouseMove({clientX: e.touches[0].clientX, clientY: e.touches[0].clientY});};
-    }
-}
-function onMouseMove(e) {
-    if (typeof e === 'undefined' || typeof e.clientX === 'undefined') {
-        return;
-    }
-    var xm = (e.clientX - Math.floor(window.innerWidth / 2)) * 0.4;
-    var ym = (e.clientY - Math.floor(window.innerHeight / 3)) * 0.4;
-    var d = Math.round(Math.sqrt(xm*xm + ym*ym) / 5);
-    text.style.textShadow = -xm + 'px ' + -ym + 'px ' + (d + 10) + 'px black';
-    if (boxProperty) {
-        box.style[boxProperty] = '0 ' + -ym + 'px ' + (d + 30) + 'px black';
-    }
-    xm = e.clientX - Math.floor(window.innerWidth / 2);
-    ym = e.clientY - Math.floor(window.innerHeight / 2);
-    spot.style.backgroundPosition = xm + 'px ' + ym + 'px';
-}
-</script> /* 
-
-<p>
-   
-</p>
-<center>
- 
-<div align="center"> 
-#outerCircleText {
-/* Optional - DO NOT SET FONT-SIZE HERE, SET IT IN THE SCRIPT */
-font-style: Electrofied;
-font-weight: bold;
-font-family: 'Electrofied', Electrofied, Electrofied;
-color: white;
-/* End Optional */
-
-/* Start Required - Do Not Edit */
-position: absolute;top: 0;left: 0;z-index: 3000;cursor: default;}
-#outerCircleText div {position: relative;}
-#outerCircleText div div {position: absolute;top: 0;left: 0;text-align: center;}
-/* End Required */
-/* End Circle Text Styles */
-</style> 
-
-</center>
 </body>
+</html>
