8f728ad2cadd12ddedd3faa18d54e8dd629a9fd6
ZemarKhos <zshadow@protonmail.ch>
2020-06-02 13:07:51 +0300

    Update webmas.php

--- webmas.php
+++ webmas.php
@@ -2,10 +2,9 @@
 <html>
 <body background="http://i.hizliresim.com/mzpZny.jpg">
 <head>
-<title>Anonymous - Zemarchus</title>
+<title>Anonymous - #OpJustice4Floyd</title>
 </head>
 <div>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 <div style="text-align:center"><img src="http://i.hizliresim.com/j2pRkJ.png" width="350" height="350" ></div>
 <br/><br/>
     <center>
@@ -15,7 +14,6 @@
 <h2>We Are Anonymous, We Are Legion.</h2>
 <h2>We Do Not Forget, We DO Not Forgive</h2>
 <h5>Expect US</h5>
-<h5>Zemarchus was here</h5>
 </center>
 <iframe width="1" height="1" src="https://www.youtube.com/embed/yVIRcnlRKF8?rel=0&autoplay=1&loop=1&playlist=yVIRcnlRKF8" frameborder="0" allowfullscreen></iframe></style>
 </div>
